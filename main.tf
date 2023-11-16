752d91f9c20946156a3bf31e81514a1f68601c02
serge fotie <fotieserge@yahoo.fr>
2023-11-15 20:28:54 -0500

    added ec2 module

--- main.tf
+++ main.tf
@@ -11,4 +11,31 @@ module "rds" {
   vpc_id = module.vpc.vpc_id
   vpc_cidr = "10.0.0.0/16"
   }
+
+  # module "security-gp" {
+  #   source = "./security-gp"
+  #   vpc_id = module.vpc.vpc_id
+  #   tags = local.project_tags
+  #   cidr_block = "0.0.0.0/0"  
+  # }
+
+# module "webserver" {
+#   source = "./webserver"
+#   security_groups = module.security-gp.security_gp_id
+#   subnet_id = module.vpc.public_subnet1_id
+#   tags = local.project_tags 
+# }
+
+# module "s3" {
+#   source = "./s3"
+#   tags = local.project_tags
   
+# }
+
+module "ec2" {
+  source = "./ec2"
+  subnet_id = module.vpc.public_subnet1_id
+  vpc_id = module.vpc.vpc_id
+  tags = local.project_tags
+  
+}
